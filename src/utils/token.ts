c9efd6802eeae4ecdc138fd6d133712b593c780d
Louis <andrewng23@proton.me>
2024-02-17 16:41:43 -0700

    cancellig passing the JWT

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,10 +1,17 @@
-const TOKEN = "token";
+// const TOKEN = "token";
 
-export const setToken = (token: string) => localStorage.setItme(TOKEN, token);
+// export const setToken = (token: string) => localStorage.setItme(TOKEN, token);
+
+// export const getToken = () => {
+//   const token = localStorage.getItem(TOKEN);
+//   return token ? `Bearer ${token}` : "";
+// };
+
+// export const clearToken = () => localStorage.clear();
+export const setToken = (token: string) => {};
 
 export const getToken = () => {
-  const token = localStorage.getItem(TOKEN);
-  return token ? `Bearer ${token}` : "";
+  return "";
 };
 
 export const clearToken = () => localStorage.clear();
