022a59409d88ba095a3f36aef0d50672e16a1431
Louis <andrewng23@proton.me>
2024-03-05 19:21:37 -0700

    enable auto scroll when we at the bottom again

--- src/components/chat-list/ChatList.tsx
+++ src/components/chat-list/ChatList.tsx
@@ -93,6 +93,115 @@
 
 // export default ChatList;
 
+// import { useEffect, useState, useRef } from "react";
+// import ChatListItem from "./chat-list-item/ChatListItem";
+// import { Box, Divider, Stack } from "@mui/material";
+// import ChatListHeader from "./chat-list-header/ChatListHeader";
+// import ChatListAdd from "./chat-list-add/ChatListAdd";
+// import { useGetChats } from "../../hooks/useGetChats";
+// import { usePath } from "../../hooks/usePath";
+// import { useMessageCreated } from "../../hooks/useMessageCreated";
+// import { PAGE_SIZE } from "../../constants/page-size";
+// import InfiniteScroll from "react-infinite-scroller";
+// import { useCountChats } from "../../hooks/useCountChats";
+
+// const ChatList = () => {
+//   const [chatListAddVisible, setChatListAddVisible] = useState(false);
+//   const [selectedChatId, setSelectedChatId] = useState("");
+//   const { data, fetchMore } = useGetChats({
+//     skip: 0,
+//     limit: PAGE_SIZE,
+//   });
+//   const { path } = usePath();
+//   const { chatsCount, countChats } = useCountChats();
+
+//   useEffect(() => {
+//     countChats();
+//   }, [countChats]);
+
+//   useMessageCreated({ chatIds: data?.chats.map((chat) => chat._id) || [] });
+
+//   useEffect(() => {
+//     const pathSplit = path.split("chats/");
+//     if (pathSplit.length === 2) {
+//       setSelectedChatId(pathSplit[1]);
+//     }
+//   }, [path]);
+
+//   const chatListRef = useRef<HTMLDivElement>(null);
+
+//   useEffect(() => {
+//     if (chatListRef.current) {
+//       const isScrolledToBottom =
+//         chatListRef.current.scrollHeight - chatListRef.current.clientHeight <=
+//         chatListRef.current.scrollTop + 1; // Add a small tolerance (e.g., 1px)
+
+//       if (isScrolledToBottom) {
+//         chatListRef.current.scrollTop = chatListRef.current.scrollHeight;
+//       }
+//     }
+//   }, [data?.chats.length]);
+
+//   return (
+//     <>
+//       <ChatListAdd
+//         open={chatListAddVisible}
+//         handleClose={() => setChatListAddVisible(false)}
+//       />
+//       <Stack>
+//         <ChatListHeader handleAddChat={() => setChatListAddVisible(true)} />
+//         <Divider />
+//         <Box
+//           ref={chatListRef}
+//           sx={{
+//             width: "100%",
+//             bgcolor: "background.paper",
+//             maxHeight: "80vh",
+//             overflow: "auto",
+//           }}
+//         >
+//           <InfiniteScroll
+//             pageStart={0}
+//             loadMore={() =>
+//               fetchMore({
+//                 variables: {
+//                   skip: data?.chats.length,
+//                 },
+//               })
+//             }
+//             hasMore={
+//               data?.chats && chatsCount ? data.chats.length < chatsCount : false
+//             }
+//             useWindow={false}
+//           >
+//             {data?.chats &&
+//               [...data.chats]
+//                 .sort((chatA, chatB) => {
+//                   if (!chatA.latestMessage) {
+//                     return -1;
+//                   }
+//                   return (
+//                     new Date(chatA.latestMessage?.createdAt).getTime() -
+//                     new Date(chatB.latestMessage?.createdAt).getTime()
+//                   );
+//                 })
+//                 .map((chat) => (
+//                   <ChatListItem
+//                     key={chat._id}
+//                     chat={chat}
+//                     selected={chat._id === selectedChatId}
+//                   />
+//                 ))
+//                 .reverse()}
+//           </InfiniteScroll>
+//         </Box>
+//       </Stack>
+//     </>
+//   );
+// };
+
+// export default ChatList;
+
 import { useEffect, useState, useRef } from "react";
 import ChatListItem from "./chat-list-item/ChatListItem";
 import { Box, Divider, Stack } from "@mui/material";
@@ -114,6 +223,8 @@ const ChatList = () => {
   });
   const { path } = usePath();
   const { chatsCount, countChats } = useCountChats();
+  const [shouldAutoScroll, setShouldAutoScroll] = useState(true);
+  const chatListRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     countChats();
@@ -128,19 +239,30 @@ const ChatList = () => {
     }
   }, [path]);
 
-  const chatListRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const chatListElement = chatListRef.current;
+    if (chatListElement) {
+      const handleScroll = () => {
+        const isScrolledToBottom =
+          chatListElement.scrollHeight - chatListElement.clientHeight <=
+          chatListElement.scrollTop + 1;
+        setShouldAutoScroll(isScrolledToBottom);
+      };
+
+      chatListElement.addEventListener("scroll", handleScroll);
+
+      // Return a cleanup function to remove the event listener
+      return () => {
+        chatListElement.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, []);
 
   useEffect(() => {
-    if (chatListRef.current) {
-      const isScrolledToBottom =
-        chatListRef.current.scrollHeight - chatListRef.current.clientHeight <=
-        chatListRef.current.scrollTop + 1; // Add a small tolerance (e.g., 1px)
-
-      if (isScrolledToBottom) {
-        chatListRef.current.scrollTop = chatListRef.current.scrollHeight;
-      }
+    if (shouldAutoScroll && chatListRef.current) {
+      chatListRef.current.scrollTop = chatListRef.current.scrollHeight;
     }
-  }, [data?.chats.length]);
+  }, [data?.chats.length, shouldAutoScroll]);
 
   return (
     <>
