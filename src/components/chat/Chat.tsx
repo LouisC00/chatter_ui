3f07099c6b2eb37510401aa9b29b9e25aa908556
Louis <andrewng23@proton.me>
2024-03-21 19:46:10 -0600

    cancle the overflow to test the flexgrow

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -102,7 +102,7 @@ const Chat = () => {
         }}
         ref={scrollContainerRef}
       >
-        {/* <Box sx={{ flexGrow: 1, minHeight: "1px" }}>flexGrow:1 box</Box> */}
+        <Box sx={{ flexGrow: 1, minHeight: "1px" }}>flexGrow:1 box</Box>
         <div>hi</div>
         <InfiniteScroll
           pageStart={0}
